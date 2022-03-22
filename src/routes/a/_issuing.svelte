6c262add4e520ffd98f1957e697ac61df290cf74
makindele <99146821+makindele@users.noreply.github.com>
2022-03-22 01:21:17 -0500

    Update _issuing.svelte

--- src/routes/a/_issuing.svelte
+++ src/routes/a/_issuing.svelte
@@ -10,7 +10,9 @@
 
 <form on:submit|preventDefault={submit}>
   <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-headline">
-    Signing {$edition}
+    The Bold Gallery Is Issuing {$edition}
   </h3>
-  <div class="mt-2">Please wait</div>
+  <div class="mt-2">
+    Please allow for the artwork to be finalized.
+  </div>
 </form>
