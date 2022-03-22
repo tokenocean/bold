1d95396c64605bbac994070ca71029adecf1c0e2
makindele <99146821+makindele@users.noreply.github.com>
2022-03-22 01:39:15 -0500

    Update _menu.svelte

--- src/routes/[username]/_menu.svelte
+++ src/routes/[username]/_menu.svelte
@@ -19,7 +19,7 @@
         <div class="my-auto">
           <Fa icon={faUserCircle} />
         </div>
-        <div><span>Apply To Be a Creator</span></div>
+        <div><span>Become a Creator</span></div>
       </div>
     </a>
   {/if}
